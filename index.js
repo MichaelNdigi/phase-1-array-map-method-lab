29564e7f46d309b00aac5e2472c5a8b0f01c52e2
Michael Ndigi <michael.ndigi@student.moringaschool.com>
2022-08-18 01:48:06 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  const cased = tutorials.map((ab) => {
+    const newCase = ab
+    .split(' ')
+    .map((word) => {
+    return word.charAt(0).toUpperCase() + word.slice(1)
+    })
+    .join(' ')
+
+      return newCase
+  })
+  return cased
 }
